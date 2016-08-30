1700d9b941d63682cb6f77f6d0a3c47c7e2360d0
Yuliana <yuliana.perez@azka.it.com>
2016-08-30 17:06:32 -0500

    ejb proyectos

--- src/main/java/mx/azka/controlDeEquipos/controller/ProyectoController.java
+++ src/main/java/mx/azka/controlDeEquipos/controller/ProyectoController.java
@@ -1,18 +1,38 @@
-
 package mx.azka.controlDeEquipos.controller;
 
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
 import mx.azka.controlDeEquipos.ejb.CeProyectoFacadeLocal;
 import mx.azka.controlDeEquipos.entity.CeProyecto;
 
-@EJB (name="ProyectoController")
+
+@Named
+@ViewScoped
 public class ProyectoController implements Serializable{
+    
+    @EJB
     private CeProyectoFacadeLocal proyectoEjb;
     private CeProyecto ceProyecto;
+
+    public CeProyecto getCeProyecto() {
+        return ceProyecto;
+    }
+
+    public void setCeProyecto(CeProyecto ceProyecto) {
+        this.ceProyecto = ceProyecto;
+    }
+    
+    @PostConstruct
+    public void init(){
+        ceProyecto = new CeProyecto();
+    }
     
     public void registrar(){
         try{
+            System.out.println("entra a registrar");
             proyectoEjb.create(ceProyecto);
         }catch(Exception e){
             
