2b6d33ad38cd622cb5434a533e81993021347550
Jorge <jorge.vargas@azka-it.com>
2016-09-07 11:20:57 -0500

    cambios en controlador equipo

--- src/main/java/mx/azka/controlDeEquipos/controller/DetalleEquipoController.java
+++ src/main/java/mx/azka/controlDeEquipos/controller/DetalleEquipoController.java
@@ -1,51 +1,91 @@
 package mx.azka.controlDeEquipos.controller;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import mx.azka.controlDeEquipos.ejb.CeEmpleadoFacadeLocal;
 import mx.azka.controlDeEquipos.ejb.CeEquiposFacadeLocal;
+import mx.azka.controlDeEquipos.entity.CeEmpleado;
 import mx.azka.controlDeEquipos.entity.CeEquipos;
 
 @Named
 @ViewScoped
-public class DetalleEquipoController {
+public class DetalleEquipoController implements Serializable{
+    
  @EJB
     private CeEquiposFacadeLocal equiposEjb;
+ @EJB
+    private CeEmpleadoFacadeLocal empleadoEjb;
     private CeEquipos equipos;
+    private CeEmpleado ceEmpelado;
 
+    public CeEmpleado getCeEmpelado() {
+        return ceEmpelado;
+    }
+
+    public void setCeEmpelado(CeEmpleado ceEmpelado) {
+        this.ceEmpelado = ceEmpelado;
+    }
+   
+    private List< SelectItem> listEmpleados;
     public CeEquipos getEquipos() {
         return equipos;
     }
+    
 
-    public void setEquipos(CeEquipos equipos) {
+  
+    
+ public List<SelectItem> getAllempleados() {
+            List<CeEmpleado> al = empleadoEjb.findAll();
+            listEmpleados = new ArrayList< SelectItem>();
+            int i = 0;
+            for (CeEmpleado empleado: al) {
+                listEmpleados.add(new SelectItem(i, empleado.getEmpNombre()+ " "+ empleado.getEmpAppPaterno()+" "+empleado.getEmpAppMaterno()));
+                i++;
+            }
+            return listEmpleados;
+    }
+ 
+ 
+     public void setEquipos(CeEquipos equipos) {
         this.equipos = equipos;
     }
       
     @PostConstruct
     public void init(){
         equipos = new CeEquipos();
+        ceEmpelado = new CeEmpleado();
+        
     }
-    
- 
- 
-  public void editar(){
+
+  public void guardar(){
         try{
-            System.out.println("entra a registrar");
+      
             equiposEjb.edit(equipos);
+   
         }catch(Exception e){
             //
         }
     }
- public void remove(){
+ public void eliminar(){
         try{
-            System.out.println("entra a registrar");
+         
             equiposEjb.remove(equipos);
         }catch(Exception e){
             //
         }
+        
+     
+
     }
  
+
+	
  
  
     
